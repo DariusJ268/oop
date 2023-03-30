771576caeb786681c8a20602a5d1c2ae97ea6998
DariusJ268 <darius.juralevic@gmail.com>
2023-03-30 20:30:33 +0300

    updates

--- Animal.php
+++ Animal.php
@@ -15,8 +15,14 @@ class Animal {
 
     public function intro()
     {
-        
+        echo "Sveiki, aš esu " . $this->species . " " . $this->name . ". Man yra " . $this->year . "metų(ai), ir aš esu " .
+      ( $this->gender ? "berniukas" : "mergaitė" )
+     . "<br>";
     }
+
+    public static function classInfo(){
+        echo "Šita klasė yra skirta sukurti gyvūnams<br>";
+      }
 
     // jei funkcijoje yra bent vienas $this - funkcija dinamine. Dinamines funkcijas kvieciame per klases objekta; statines per klase
 }
